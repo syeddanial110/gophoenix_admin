a161370fe863d265e9bfc0a73c44e4bfc73ffe52
Syed Danial <daniald3200@gmail.com>
2026-05-22 06:57:52 +0500

    inital url redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
 "use client";
 
+import { pathLocations } from "@/utils/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    // This will run only on the client side
+    router.push(`${pathLocations.dashboard}`)
+  }, []);
   return (
     <div className="flex flex-col items-center h3 pt-10 gap-3">
      
